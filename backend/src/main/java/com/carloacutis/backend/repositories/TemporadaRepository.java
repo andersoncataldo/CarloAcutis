789004d82ca417efc2b85b17c7b15e5e8e5b2fa5
Anderson Cataldo <andersonvcataldo@gmail.com>
2026-08-13 23:50:48 -0300

    correcoes de push do projeto

--- backend/src/main/java/com/carloacutis/backend/repositories/TemporadaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.carloacutis.backend.repositories;
-
-import com.carloacutis.backend.models.Temporada;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TemporadaRepository extends JpaRepository<Temporada, Long> {
-}
